f07c1872fca326170a83958017dc5972114c61fb
KAD78 <gerarmante4jui86@gmail.com>
2026-03-22 08:47:46 +0300

    Initialize main.py with project details and setup
    Un outil de scan professionnel, structuré, rapide, avec analyse avancée des réseaux + dashboard CLI + enrichissement CVE (API publique)
    👉 sans fonctions offensives illégales (pas de bruteforce / exploitation)
    
    🚀 🔥 CE QUE TU VOUS ALLEZ AVOIR : 
    
    ✅ Scan ultra rapide (threads)
    ✅ Multi-IP / CIDR
    ✅ Détection services + risques
    ✅ Enrichissement CVE (API NVD)
    ✅ OSINT IP
    ✅ Score vulnérabilité
    ✅ Export JSON / CSV / PDF
    ✅ Interface CLI stylée
    ✅ Code modulaire propre.
    
    
    ⚠️ AVANT DE COMMENCER
    
    Installe dépendances :
    
    pkg install nmap curl
    pip install reportlab requests

---
+++ main.py
@@ -0,0 +1,27 @@
+"""
+
+Un outil de scan professionnel, structuré, rapide, avec analyse avancée des réseaux + dashboard CLI + enrichissement CVE (API publique)
+👉 sans fonctions offensives illégales (pas de bruteforce / exploitation)
+
+🚀 🔥 CE QUE TU VOUS ALLEZ AVOIR : 
+
+✅ Scan ultra rapide (threads)
+✅ Multi-IP / CIDR
+✅ Détection services + risques
+✅ Enrichissement CVE (API NVD)
+✅ OSINT IP
+✅ Score vulnérabilité
+✅ Export JSON / CSV / PDF
+✅ Interface CLI stylée
+✅ Code modulaire propre
+
+⚠️ AVANT DE COMMENCER
+
+Installe dépendances :
+
+pkg install nmap curl
+pip install reportlab requests
+
+"""
+
+
